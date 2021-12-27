070508369b0430855491dc6477f60ed86aad0570
velagalamohan <velagalamohan@gmail.com>
2021-12-27 07:35:32 +0530

    hello

---
+++ src/org/add/Client.java
@@ -0,0 +1,15 @@
+package org.add;
+
+public class Client {
+	
+	public static void tester() {
+System.out.println("mohan is a tester");
+
+	}
+	
+	public static void main(String[] args) {
+		tester();
+		
+	}
+
+}
